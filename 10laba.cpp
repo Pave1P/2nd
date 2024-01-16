27786d4139e9b734f4ac8b31b6a7d09e9fcda12a
Pave1P <145470076+Pave1P@users.noreply.github.com>
2024-01-16 19:35:34 +0200

    Add files via upload

---
+++ 10laba.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	vector <int> vec(n);
+	for (int i = 0; i < n; i++) {
+		cin >> vec[i];
+	}
+	vec.push_back(1); // Добавляем элемент 1 в конец вектора
+	vec.push_back(3); // Добавляем элемент 2 в конец вектора
+
+	vec.erase(vec.begin()); // Удаляем первый элемент вектора
+
+	vec.insert(vec.begin(), 3); // Вставляем элемент 3 в начало вектора
+
+	for (int i = 0; i < n; i++) {
+		cout << vec[i];
+	}
+	vec.clear(); // Очищаем вектор
+}
+	
