e3dfb61036e65c4717599663dd56e1db6026372b
Pave1P <145470076+Pave1P@users.noreply.github.com>
2024-01-23 18:06:33 +0200

    Add files via upload

---
+++ 2 laba.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+class Rectangle {
+private:
+    float width;
+    float height;
+
+public:
+    Rectangle(float w, float h) : width(w), height(h) {}
+
+    float getArea() {
+        return width * height;
+    }
+
+    float getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    float getDiagonal() {
+        return sqrt(pow(width, 2) + pow(height, 2));
+    }
+};
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    Rectangle rect(3, 3);
+
+    std::cout << "Площадь: " << rect.getArea() << std::endl;
+    std::cout << "Периметр: " << rect.getPerimeter() << std::endl;
+    std::cout << "Длина диагонали: " << rect.getDiagonal() << std::endl;
+
+    return 0;
+}
