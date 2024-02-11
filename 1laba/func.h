1e8c2cb3bcc960648bea89842d48c60207092f14
Pave1P <145470076+Pave1P@users.noreply.github.com>
2024-02-11 13:04:56 +0200

    Add files via upload

---
+++ 1laba/func.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef FUNC_HPP
+#define FUNC_HPP
+#include <string>
+
+namespace FileIO {
+    void appendTimeToFile(const std::string& filename);
+}
+
+#endif
---
+++ 1laba/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "func.h"
+
+int main() {
+    FileIO::appendTimeToFile("log.txt");
+
+    return 0;
+}
